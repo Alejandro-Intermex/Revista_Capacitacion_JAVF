f498b1587c9056cf5e687cad7c04218650ec1d31
DesIntermex2023 <desarrollointermex2023@outlook.com>
2023-06-22 10:50:21 -0600

    aa

---
+++ Revista_Capacitacion/Models/ClsRegistro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Revista_Capacitacion.Models
+{
+    public class ClsRegistro
+    {
+        public string titulo { get; set; }
+        public string CB { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cat_ID { get; set; }
+        public DateTime fechaReg { get; set; }
+        public float precio { get; set; }
+    }
+}
