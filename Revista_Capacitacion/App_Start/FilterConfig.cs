9f63d6be33d07c8f6756ac4ae064ca68913280f7
DesIntermex2023 <desarrollointermex2023@outlook.com>
2023-06-22 08:31:05 -0600

    Agregar archivos de proyecto.

---
+++ Revista_Capacitacion/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Revista_Capacitacion
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
