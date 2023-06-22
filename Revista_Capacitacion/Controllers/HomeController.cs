18db7cc9232750186369c0a522c27b67ba6ca45a
DesIntermex2023 <desarrollointermex2023@outlook.com>
2023-06-22 13:36:31 -0600

    cambios

--- Revista_Capacitacion/Controllers/HomeController.cs
+++ Revista_Capacitacion/Controllers/HomeController.cs
@@ -8,23 +8,63 @@ namespace Revista_Capacitacion.Controllers
 {
     public class HomeController : Controller
     {
+        CAPACITACIONEntities5 _context = new CAPACITACIONEntities5();
         public ActionResult Index()
         {
-            return View();
+            var listofData = _context.Employees.ToList();
+            return View(listofData);
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Crear()
+        [HttpPost]
+        public ActionResult Create(REVISTAS model)
         {
-            ViewBag.Message = "Your contact page.";
-
+            _context.Employees.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
             return View();
         }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(REVISTAS Model)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == Model.ID_REV).FirstOrDefault();
+            if (data != null)
+            {
+                data.EmployeeCity = Model.TITULO_REV;
+                data.EmployeeName = Model.CB;
+                data.EmployeeSalary = Model.FECHA_CIRCULACION;
+                data.Employeeidcat = Model.ID_CAT;
+                data.Employeerow = Model.ROW_CREATE;
+                data.Employeeprecio = Model.PRECIO;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+            _context.Employees.Remove(data);
+            _context.SaveChanges();
+            ViewBag.Messsage = "Record Delete Successfully";
+            return RedirectToAction("index");
+        }
+        public ActionResult Detail(int id)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+            return View(data);
+        }
+
     }
 }
